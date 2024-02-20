e1c1ae144340d54c918ecc2d0b095a6831033e33
hakuunabatata <emachadomartins1999@gmail.com>
2024-02-19 22:17:37 -0300

    feat: create route ping

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	g "github.com/gin-gonic/gin"
+)
+
 func main() {
-	print("Go\n")
+	r := g.Default()
+
+	r.GET("/ping", func(ctx *g.Context) {
+		ctx.JSON(200, g.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
