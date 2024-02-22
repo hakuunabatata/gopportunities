b739187e42f7156778c336be7316bf6f064814ac
hakuunabatata <emachadomartins1999@gmail.com>
2024-02-21 21:05:39 -0300

    feat: creat handler module

--- router/routes.go
+++ router/routes.go
@@ -1,12 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	gin "github.com/gin-gonic/gin"
+	handler "github.com/hakuunabatata/gopportunities/handler"
+)
 
 func initRoutes(router *gin.Engine) {
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
+	v1 := router.Group("/api/v1/")
+	{
+		v1.GET("/opportunity", handler.ShowOpeningHandler)
+		v1.POST("/opportunity", handler.CreateOpeningHandler)
+		v1.PUT("/opportunity", handler.UpdateOpeningHandler)
+		v1.DELETE("/opportunity", handler.DeleteOpeningHandler)
+		v1.GET("/opportunities", handler.ListOpeningHandler)
+	}
 }
