98f501d99c7b599bdb7681ad748ceeff395598dd
hakuunabatata <emachadomartins1999@gmail.com>
2024-02-26 18:34:30 -0300

    feat: create logger

--- config/config.go
+++ config/config.go
@@ -5,9 +5,15 @@ import (
 )
 
 var (
-	db *gorm.DB
+	db     *gorm.DB
+	logger *Logger
 )
 
 func Init() error {
 	return nil
+}
+
+func GetLogger(p string) *Logger {
+	logger = NewLogger(p)
+	return logger
 }
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	fmt "fmt"
-
 	config "github.com/hakuunabatata/gopportunities/config"
 	router "github.com/hakuunabatata/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
 	err := config.Init()
 	if err != nil {
-		fmt.Println(err)
+		logger.Error(err)
 		return
 	}
 
